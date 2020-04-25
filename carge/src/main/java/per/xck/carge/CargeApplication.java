0a810494ae48392c6b41e70c5c576d0db0ebb9ea
XCKjacksparrow <jackparrow@smail.hyit.edu.cn>
2020-04-25 20:14:55 +0800

    init

---
+++ carge/src/main/java/per/xck/carge/CargeApplication.java
@@ -0,0 +1,13 @@
+package per.xck.carge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargeApplication.class, args);
+    }
+
+}
---
+++ carge/src/test/java/per/xck/carge/CargeApplicationTests.java
@@ -0,0 +1,13 @@
+package per.xck.carge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
